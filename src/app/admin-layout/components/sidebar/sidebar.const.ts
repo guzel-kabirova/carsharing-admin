ac19b165998452bccdc8c61f3735621830dfbf19
guzel-kabirova <vkcom1998@gmail.com>
2022-04-19 22:42:01 +0300

    V-09 feat: Add adaptive

--- src/app/admin-layout/components/sidebar/sidebar.const.ts
+++ src/app/admin-layout/components/sidebar/sidebar.const.ts
@@ -1,7 +1,7 @@
 export const MENU_ITEMS = [
   {name: 'Карточка автомобиля', link: 'card', iconName: 'edit'},
   {name: 'Список авто', link: 'list', iconName: 'list'},
-  {name: 'Зказы', link: 'orders', iconName: 'order'},
+  {name: 'Заказы', link: 'orders', iconName: 'order'},
   {name: 'Menu 4', link: 'four', iconName: 'menu-cards-six'},
   {name: 'Menu 5', link: 'five', iconName: 'menu-cards-five'},
   {name: 'Menu 6', link: 'six', iconName: 'person'},
